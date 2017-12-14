24cfe26634eac0ad75f66d20ea4416a2ca00d1ef
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2017-12-14 13:34:16 -0700

    Added function for our number arrays

--- app/scripts/math.js
+++ app/scripts/math.js
@@ -1,4 +1,13 @@
 var kylesArray = [44, 42, 36, 2]
 
-let numberArray = [5, 3, 2, 9];
+let numberArray = [5, 3, 2, 9]
 
+function arrayMultipy(ar1, ar2){
+  var totals = []
+  for (var i = 0; i < ar1.length; i++){
+    totals.push(ar1[i] * ar2[i])
+  }
+  return totals
+}
+
+arrayMultipy(kylesArray, numberArray)
